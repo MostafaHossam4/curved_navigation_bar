15aba3a08601d26c2724f996ada5d19e2e5207d0
mostafa <mostafa.hossam@grandtk.com>
2021-10-31 21:45:04 +0200

    first commit

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -129,7 +129,7 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
           Positioned(
             left: 0,
             right: 0,
-            bottom: 0 - (75.0 - widget.height),
+            bottom: 0,
             child: CustomPaint(
               painter: NavCustomPainter(
                   _pos, _length, widget.color, Directionality.of(context)),
@@ -141,7 +141,7 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
           Positioned(
             left: 0,
             right: 0,
-            bottom: 10,
+            bottom: 0 - (75.0 - widget.height),
             child: SizedBox(
                 height: 100.0,
                 child: Row(
